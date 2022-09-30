6a3702ce9c87eff85a6351b8d982f536c010cf48
Sathira443 <liyanapathiranasathira@gmail.com>
2022-09-30 08:38:41 +0530

    new model classes

---
+++ lib/Model/food_item_model.dart
@@ -0,0 +1,18 @@
+class FoodItemModel {
+  // int food_id;
+  String name;
+  int state;
+  int calory_count;
+  int carbs_count;
+  int fat_count;
+  int protein_count;
+
+  FoodItemModel(
+    this.name,
+    this.state,
+    this.calory_count,
+    this.carbs_count,
+    this.fat_count,
+    this.protein_count,
+  );
+}
